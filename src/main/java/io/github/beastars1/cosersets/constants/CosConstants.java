74efe3f6c4a65e5b4a210ca2a6564944e758cbaf
beastars1 <1229435236@qq.com>
2021-08-18 18:21:08 +0800

    get and download webp

---
+++ src/main/java/io/github/beastars1/cosersets/constants/CosConstants.java
@@ -0,0 +1,11 @@
+package io.github.beastars1.cosersets.constants;
+
+public interface CosConstants {
+  String DOMAIN = "www.cosersets.com";
+
+  String PREFIX = "/1/main";
+
+  String FILE = "FILE";
+
+  String FOLDER = "FOLDER";
+}
